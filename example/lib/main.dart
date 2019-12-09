42ab0a1cd0acbe8e3a000a0b69db1aa278245460
Yahhi <vkonuhova@gmail.com>
2019-12-09 16:21:15 +0300

    make it work on iOS, Android, web, change example selection (now shows one example at a time)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:example_roller_list/slot_machine.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'clock_selector.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  if (kIsWeb) {
+    runApp(new MyApp());
+  } else {
+    WidgetsFlutterBinding.ensureInitialized();
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+        .then((_) async {
+      runApp(new MyApp());
+    });
+  }
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -11,38 +23,68 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'RollerList Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int selectedItem = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("RollerList Demo"),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              ClockSelector(),
-              SizedBox(
-                height: 16.0,
+        title: Text("RollerList"),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            decoration: BoxDecoration(
+              color: Colors.white10,
+              border: Border.all(
+                  width: 1.0,
+                  style: BorderStyle.solid,
+                  color: Colors.redAccent),
+            ),
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton<int>(
+                value: selectedItem,
+                items: <DropdownMenuItem<int>>[
+                  new DropdownMenuItem(
+                    child: new Text(
+                      'CLOCK',
+                      textAlign: TextAlign.right,
+                    ),
+                    value: 0,
+                  ),
+                  new DropdownMenuItem(
+                    child: new Text(
+                      'SLOT MACHINE',
+                    ),
+                    value: 1,
+                  ),
+                ],
+                onChanged: (int value) {
+                  setState(() => selectedItem = value);
+                },
               ),
-              Divider(),
-              SizedBox(
-                height: 16.0,
-              ),
-              SlotMachine(),
-            ],
+            ),
           ),
-        ),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child:
+            Center(child: selectedItem == 0 ? ClockSelector() : SlotMachine()),
       ),
     );
   }
